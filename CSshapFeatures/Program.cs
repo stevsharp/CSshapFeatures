52a753081451a22f54ce731d1de546b51df5b486
SPYROS\Spyros <sponaris@gmail.com>
2024-08-28 11:52:13 +0300

    Add project files.

---
+++ CSshapFeatures/Program.cs
@@ -0,0 +1,55 @@
+﻿
+class Program
+{
+    static async Task Main(string[] args)
+    {
+
+        using (CancellationTokenSource cts = new CancellationTokenSource())
+        {
+            CancellationToken token = cts.Token;
+
+            var task = Task.Run(() => DoWork(token), token);
+
+            Console.WriteLine("Press 'c' to cancel the operation.");
+
+            if (Console.ReadKey(true).KeyChar == 'c')
+            {
+                cts.Cancel();
+                Console.WriteLine("Cancellation requested.");
+            }
+
+            try
+            {
+                await task;
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Operation was canceled.");
+            }
+            finally
+            {
+                cts.Dispose();
+            }
+
+        }
+
+        Console.ReadLine();
+    }
+
+    static async Task DoWork(CancellationToken token)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            if (token.IsCancellationRequested)
+            {
+                Console.WriteLine("Cancellation requested inside the task.");
+
+                token.ThrowIfCancellationRequested();
+            }
+
+            Console.WriteLine($"Working... {i + 1}/10");
+
+            await Task.Delay(1000, token);
+        }
+    }
+}
