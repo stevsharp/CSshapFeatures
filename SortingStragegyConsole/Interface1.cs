466774c075f9355ed7cac564e8d8642cce021836
SPYROS\Spyros <sponaris@gmail.com>
2024-09-09 11:05:20 +0300

    SortingStragegyConsole

---
+++ SortingStragegyConsole/Interface1.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Collections;
+using System.Collections.Immutable;
+
+namespace SortingStragegyConsole;
+
+public interface ISortStrategy
+{
+    IOrderedEnumerable<string> Sort(IEnumerable<string> input);
+}
+
+public class SortAscendingStrategy : ISortStrategy
+{
+    public IOrderedEnumerable<string> Sort(IEnumerable<string> input) => input.OrderBy(x => x);
+    
+}
+
+
+public class SortDescingStrategy : ISortStrategy
+{
+    public IOrderedEnumerable<string> Sort(IEnumerable<string> input) => input.OrderByDescending(x => x);
+
+}
+
+public class SortableCollection(IEnumerable<string> items) : IEnumerable<string>
+{
+
+    private ISortStrategy _sortStrategy = new SortAscendingStrategy();
+
+    private ImmutableArray<string> _items = items.ToImmutableArray();
+    public IEnumerable<string> Items => _items;
+    public void SetSortStrategy(ISortStrategy strategy)=> _sortStrategy = strategy;
+    public void Sort() => _items = _sortStrategy.Sort(_items).ToImmutableArray();
+    public IEnumerator<string> GetEnumerator()=> Items.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator()=> ((IEnumerable)Items).GetEnumerator();
+}
---
+++ SortingStragegyConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using SortingStragegyConsole;
+
+try
+{
+    SortableCollection data = new(new[] {"Lorem", "ipsum", "dolor", "sit", "amet." });
+
+    // User inputs the sorting order
+    Console.WriteLine("Enter sorting order (A/D):");
+    string sortOrderInput = Console.ReadLine().Trim().ToLower();
+
+    // Determine the sorting strategy based on user input
+    ISortStrategy strategy = sortOrderInput == "A"
+        ? new SortAscendingStrategy()
+        : new SortDescingStrategy(); 
+
+    // Set and perform the sorting
+    data.SetSortStrategy(strategy);
+    data.Sort();
+
+    // Display the sorted result
+    Console.WriteLine("Sorted data:");
+    foreach (var item in data.Items)
+    {
+        Console.WriteLine(item);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occurred: " + ex.Message);
+}
+
+Console.ReadLine();
